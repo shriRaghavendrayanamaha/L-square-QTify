b9a247416528b1369dd406314ec27a6319761b59
shriRaghavendrayanamaha <sandeepkumaryali@gmail.com>
2025-06-29 20:34:58 +0530

    qtify coded

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
+import HomePage from "./pages/HomePage/HomePage";
 import reportWebVitals from './reportWebVitals';
-
+import AlbumPage from "./pages/AlbumPage/AlbumPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/album/:albumId",
+        element: <AlbumPage />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
